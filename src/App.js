f238b9238b0a73e02c2d6d7bbff9ea1d5383f068
Matthew <matthew.abugdag23@gmail.com>
2024-10-13 22:04:57 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    //BEM
     <div className="App">
       <h1>Hello Matthew</h1>
     </div>
