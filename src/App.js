421a4cc7d041cec6b7be6d0bca16971a29ff1e71
Matthew <matthew.abugdag23@gmail.com>
2024-10-13 22:32:25 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //BEM
     <div className="App">
       <h1>Hello Matthew</h1>
-      {/* Header */}
+      < Header />
       {/* Home */}
     </div>
   );
