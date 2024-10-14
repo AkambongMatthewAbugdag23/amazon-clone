afad50fd5a33cee9a19faced16911e6e8e3fec8c
Matthew <matthew.abugdag23@gmail.com>
2024-10-14 08:33:34 +0000

    a

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 
 function Header() {
     return (
         <div className="header">
-            <img className="header__logo" src="amazon-logo-1024x683.png" alt="amazon-logo"/>
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon-logo"/>
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
                 <SearchIcon className="header__searchIcon" />
@@ -23,6 +24,11 @@ function Header() {
                 <div className="header_option">
                     <span className="header_optionLineOne">Your</span>
                     <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <div className="header_optionBasket">
+                    <AddShoppingCartIcon />
+                    <span className="header_optionLineTwo 
+                    header_basketCount">0</span>
                 </div>
             </div>
         </div>
