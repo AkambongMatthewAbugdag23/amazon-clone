7aa232dddff146d5716f1bffb1b242c8c548b0c3
Matthew <matthew.abugdag23@gmail.com>
2024-10-19 09:56:28 +0000

    subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -24,7 +24,7 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
           />
-            <button>Proceed to Checkout</button>
+            <button className="subtotal_button">Proceed to Checkout</button>
     </div>
   )
 }
